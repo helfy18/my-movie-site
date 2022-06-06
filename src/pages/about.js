8ccb2c6e93c61f37dbb3e403584ab1f90cc4b0b3
Johnathan <jhelfric@uvic.ca>
2022-06-05 22:27:41 -0700

    simple styling and about page added

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+        <p>
+            Hello, our names are Johnathan and Dani, and we built this website to showcase our movie ratings.
+            We will consistently be adding new ratings as we watch them.<br/>Look out for site upgrades as 
+            well!
+        </p>
+    </Layout>
+  )
+}
+
+export default AboutPage
