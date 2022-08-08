54021421ea308f55817cdb1b7f835986397c5849
Johnathan Helfrich <johnathan@19labs.com>
2022-08-08 16:13:33 -0700

    update query, content, set up selectpicker

--- src/pages/movie-list.js
+++ src/pages/movie-list.js
@@ -15,22 +15,43 @@ const ExcelPage = ({ data }) => {
         </Layout>
     )
 }
-
+//need to merge genre with genre2
 export const query = graphql`
     query  {
+        genre: allMovieMovieMoviesXlsxMasterlist {
+          distinct(field: Genre)
+        }
+        genre_two: allMovieMovieMoviesXlsxMasterlist(filter: {Genre_2: {ne: " "}}) {
+          distinct(field: Genre_2)
+        }
+        years: allMovieMovieMoviesXlsxMasterlist {
+          distinct(field: Year)
+        }
+        universes: allMovieMovieMoviesXlsxMasterlist(filter: {Universe: {ne: ""}}) {
+          distinct(field: Universe)
+        }
+        sub_universes: allMovieMovieMoviesXlsxMasterlist(filter: {Sub_Universe: {ne: ""}}) {
+          distinct(field: Sub_Universe)
+        }
+        exclusive: allMovieMovieMoviesXlsxMasterlist(filter: {Exclusive: {ne: ""}}) {
+          distinct(field: Exclusive)
+        }
+        holiday: allMovieMovieMoviesXlsxMasterlist(filter: {Holiday: {ne: ""}}) {
+          distinct(field: Holiday)
+        }
         movies: allMovieMovieMoviesXlsxMasterlist {
-            nodes {
-                Movie
-                Score
-                Universe
-                Sub_Universe
-                Genre
-                Genre_2
-                Exclusive
-                Holiday
-                Year
-                id
-            }
+          nodes {
+            Movie
+            Score
+            Universe
+            Sub_Universe
+            Genre
+            Genre_2
+            Exclusive
+            Holiday
+            Year
+            id
+          }
         }
         second: allMovieMovieMoviesXlsxMasterlist(
           filter: {Genre: {eq: "Animated"}, Year: {eq: 1995}}
