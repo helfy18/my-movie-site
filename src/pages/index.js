e409ab8c023fc99ced6bd0ea3dc5e45c79ba1d6b
Johnathan Helfrich <johnathan@19labs.com>
2022-08-17 15:04:52 -0700

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,13 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>
-        This will be the home page for my movie website.<br/>I am building this using the Gatsby
-        tutorial.
+        This will be the home page for my movie website.<br/><br/>
+        All of the content is found above at 'Movie Ratings'.
       </p>
-      <StaticImage 
-        alt="a cute picture of the owl house"
-        src="../images/owl-house.png"/>
       </Layout>
   )
 }
