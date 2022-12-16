d3deb1e0512e85a27296b6e70410adaddd35c741
Johnathan <jhelfric@uvic.ca>
2022-12-15 21:07:12 -0700

    add if

--- src/pages/movie-page.js
+++ src/pages/movie-page.js
@@ -63,44 +63,51 @@ const gradientArray = new Gradient()
     .getColors();
 
 const MoviePage = ({location}) => {
-    return (
-      <div>
-        <Layout pageTitle = "Movies :)">
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2.5}>
-                    <Grid item xs>
-                        <img src={location.state.from.Poster} alt="Oops Something Went Wrong"></img>
-                    </Grid>
-                    <Grid item xs>
-                        <table id="infoTable" className='table'>
-                            <tbody>
-                                <tr><td>Title</td><td>{location.state.from.Movie}</td></tr>
-                                <tr><td>Score</td><td style={{color: gradientArray[location.state.from.Score], fontWeight: "bolder"}}>{location.state.from.Score}/100</td></tr>
-                                {getUniverse(location.state.from.Universe)}
-                                {getSubUniverse(location.state.from.Sub_Universe)}
-                                {getGenres(location.state.from)}
-                                {getExclusive(location.state.from.Exclusive)}
-                                {getHoliday(location.state.from.Holiday)}
-                                <tr><td>Year</td><td>{location.state.from.Year}</td></tr>
-                                <tr><td>MPA Rating</td><td>{location.state.from.Rated}</td></tr>
-                                <tr><td>Runtime</td><td>{location.state.from.Runtime}</td></tr>
-                                <tr><td>Box Office</td><td>{location.state.from.BoxOffice}</td></tr>
-                                <tr><td>Actors</td><td>{location.state.from.Actors}</td></tr>
-                                <tr><td>Director</td><td>{location.state.from.Director}</td></tr>
-                            </tbody>
-                        </table>
-                    </Grid>
-                    <Grid item xs>
-                        <Item>Plot:<br/>{location.state.from.Plot}</Item>
-                        <br/>
-                        {getReview(location.state.from.Review)}
-                    </Grid>
-                </Grid>
-            </Box>
-            {/* {JSON.stringify(location.state.from.Ratings, null, 2)} */}
-        </Layout>
-      </div>
-    )
+    if (location.state){ 
+        return (
+            <div>
+              <Layout pageTitle = "Movies :)">
+                  <Box sx={{ flexGrow: 1 }}>
+                      <Grid container spacing={2.5}>
+                          <Grid item xs>
+                              <img src={location.state.from.Poster} alt="Oops Something Went Wrong"></img>
+                          </Grid>
+                          <Grid item xs>
+                              <table id="infoTable" className='table'>
+                                  <tbody>
+                                      <tr><td>Title</td><td>{location.state.from.Movie}</td></tr>
+                                      <tr><td>Score</td><td style={{color: gradientArray[location.state.from.Score], fontWeight: "bolder"}}>{location.state.from.Score}/100</td></tr>
+                                      {getUniverse(location.state.from.Universe)}
+                                      {getSubUniverse(location.state.from.Sub_Universe)}
+                                      {getGenres(location.state.from)}
+                                      {getExclusive(location.state.from.Exclusive)}
+                                      {getHoliday(location.state.from.Holiday)}
+                                      <tr><td>Year</td><td>{location.state.from.Year}</td></tr>
+                                      <tr><td>MPA Rating</td><td>{location.state.from.Rated}</td></tr>
+                                      <tr><td>Runtime</td><td>{location.state.from.Runtime}</td></tr>
+                                      <tr><td>Box Office</td><td>{location.state.from.BoxOffice}</td></tr>
+                                      <tr><td>Actors</td><td>{location.state.from.Actors}</td></tr>
+                                      <tr><td>Director</td><td>{location.state.from.Director}</td></tr>
+                                  </tbody>
+                              </table>
+                          </Grid>
+                          <Grid item xs>
+                              <Item>Plot:<br/>{location.state.from.Plot}</Item>
+                              <br/>
+                              {getReview(location.state.from.Review)}
+                          </Grid>
+                      </Grid>
+                  </Box>
+                  {/* {JSON.stringify(location.state.from.Ratings, null, 2)} */}
+              </Layout>
+            </div>
+        )
+    }
+    else {
+        return (
+            <Layout></Layout>
+        )
+    }
 }
 
 export default MoviePage
