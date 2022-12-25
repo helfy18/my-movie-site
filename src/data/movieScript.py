924dc2636ae20f729895427bd4d5a8c1d26f12e5
Johnathan <jhelfric@uvic.ca>
2022-12-24 22:03:23 -0700

    update ratings and remove old page

--- src/data/movieScript.py
+++ src/data/movieScript.py
@@ -59,34 +59,34 @@ for index, row in enumerate(ws.iter_rows(values_only=True)):
             # if not (ws[index + 1][actors].value and ws[index + 1][boxoffice].value and ws[index+1][budget].value):
             m2 = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={config.tmdbkey}&query={t}&year={y}').json()
             tmdbcode = m2["results"][0]["id"]
-            if not (ws[index + 1][actors].value):
-                m3 = requests.get(f'https://api.themoviedb.org/3/movie/{tmdbcode}/credits?api_key={config.tmdbkey}').json()
-                actorString = ""
-                count = 1
-                for actor in m3["cast"]:
-                    if count < 8:
-                        actorString = f'{actorString}{actor["name"]}, '
-                        count = count + 1
-                    else:
-                        actorString = f'{actorString}{actor["name"]}'
-                        break
-                if actorString and actorString[-2] == ",":
-                    actorString = actorString[:-2]
-                print(t, actorString)
-                if actorString:
-                    ws[index + 1][actors].value = actorString
+            if m2["results"][0]["original_title"] == 'X-Men: First Class 35mm Special':
+                tmdbcode = m2['results'][1]['id']
+            m3 = requests.get(f'https://api.themoviedb.org/3/movie/{tmdbcode}/credits?api_key={config.tmdbkey}').json()
+            actorString = ""
+            count = 1
+            for actor in m3["cast"]:
+                if count < 8:
+                    actorString = f'{actorString}{actor["name"]}, '
+                    count = count + 1
                 else:
-                    ws[index + 1][actors].value = m["Actors"]
-            if not (ws[index + 1][boxoffice].value and ws[index + 1][budget].value):
-                m3 = requests.get(f'https://api.themoviedb.org/3/movie/{tmdbcode}?api_key={config.tmdbkey}').json()
-                try:
-                    boxofficeTotal = m3['revenue']
-                except KeyError:
-                    boxofficeTotal = 'N/A'
-                ws[index + 1][plot].value = m3["overview"]
-                ws[index + 1][boxoffice].value = f"{boxofficeTotal:,}"
-                ws[index + 1][budget].value = f"{m3['budget']:,}"
-                ws[index + 1][plot].value = m3['overview']
+                    actorString = f'{actorString}{actor["name"]}'
+                    break
+            if actorString and actorString[-2] == ",":
+                actorString = actorString[:-2]
+            print(t, actorString)
+            if actorString:
+                ws[index + 1][actors].value = actorString
+            else:
+                ws[index + 1][actors].value = m["Actors"]
+            m3 = requests.get(f'https://api.themoviedb.org/3/movie/{tmdbcode}?api_key={config.tmdbkey}').json()
+            try:
+                boxofficeTotal = m3['revenue']
+            except KeyError:
+                boxofficeTotal = 'N/A'
+            ws[index + 1][plot].value = m3["overview"]
+            ws[index + 1][boxoffice].value = f"{boxofficeTotal:,}"
+            ws[index + 1][budget].value = f"{m3['budget']:,}"
+            ws[index + 1][plot].value = m3['overview']
             # unpack values and save to spreadsheet
             ws[index + 1][poster].value = m["Poster"]
             ws[index + 1][director].value = m["Director"]
