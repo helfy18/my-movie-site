e530ee9fe5e61f5896eca00f6076ea66b899ff43
Johnathan <jhelfric@uvic.ca>
2023-01-16 22:47:46 -0800

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,8 @@ const AboutPage = () => {
         <p>
             Hello, our names are Johnathan and Dani, and we built this website to showcase our movie ratings.
             We will consistently be adding new ratings as we watch them.<br/><br/>
-            Look out for site upgrades as well!
+            Look out for site upgrades as well! Every Saturday morning MST there will be an update to the blog
+            that shows the newly added movies to the site over the past week!
         </p>
     </Layout>
   )
