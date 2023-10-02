7c988bda36f34c94af93d5d40a90ad70f358103f
Johnathan <jhelfric@uvic.ca>
2023-10-02 16:04:45 -0700

    update merge

--- src/pages/movie-grid.js
+++ src/pages/movie-grid.js
@@ -13,19 +13,7 @@ import {
 } from "../components/layout.module.css";
 import { Row, Col } from "react-grid-system";
 import SearchField from "react-search-field";
-import Grid from "@mui/material/Grid";
-import * as React from 'react'
-import { graphql } from 'gatsby'
-import { useState } from 'react';
-import Layout from '../components/layout'
-import MovieGrid from '../components/movieGrid';
-import GenerateFilter from '../components/generateFilter';
-import Select from 'react-select';
-import dataQuery from '../components/dataQuery';
-import {reactGrid, reactSelectContainer, searchBar} from '../components/layout.module.css'
-import { Row, Col } from 'react-grid-system';
-import SearchField from 'react-search-field';
-import { Slider, Grid } from '@mui/material'
+import { Slider, Grid } from "@mui/material";
 
 function searchFilter(text, data) {
   var newData = [];
@@ -51,22 +39,23 @@ function selectedOptions(data, toAdd, label) {
 }
 
 const GridPage = ({ data }) => {
-    const nodes = data.movies.nodes;
-    // Sort the data in descending score
-    nodes.sort((a, b) => {
-      return b.Score - a.Score;
-    });
-    // Parse the ratings from string to object
-    for (let index in nodes) {
-      //nodes[index] = Object.assign({"Overall Rank": i++}, nodes[index] );
-      if (typeof(nodes[index].Ratings) !== "string") {
-        try {
-          nodes[index].Ratings = JSON.parse(nodes[index].Ratings.replace(/'/g, '"'))
-        }
-        catch (err) {
-            console.log(nodes[index])
-        }
+  const nodes = data.movies.nodes;
+  // Sort the data in descending score
+  nodes.sort((a, b) => {
+    return b.Score - a.Score;
+  });
+  // Parse the ratings from string to object
+  for (let index in nodes) {
+    //nodes[index] = Object.assign({"Overall Rank": i++}, nodes[index] );
+    if (typeof nodes[index].Ratings !== "string") {
+      try {
+        nodes[index].Ratings = JSON.parse(
+          nodes[index].Ratings.replace(/'/g, '"')
+        );
+      } catch (err) {
+        console.log(nodes[index]);
       }
+    }
   }
 
   const handleChange = (event, newValue) => {
@@ -74,17 +63,17 @@ const GridPage = ({ data }) => {
   };
 
   function resetFilter() {
-    setSelected([])
-    setSliderValue([0, 1000])
+    setSelected([]);
+    setSliderValue([0, 1000]);
     setTable(dataQuery([], { data }, sliderValue));
     setShowDropdown(false);
   }
-  
-  const [showDropdown, setShowDropdown] = useState(false)
+
+  const [showDropdown, setShowDropdown] = useState(false);
   const [table, setTable] = useState(nodes);
   const [selected, setSelected] = useState(null);
-  const filter = GenerateFilter({data});
-  const [sliderValue, setSliderValue] = useState([0, 1000])
+  const filter = GenerateFilter({ data });
+  const [sliderValue, setSliderValue] = useState([0, 1000]);
 
   return (
     <div>
@@ -96,8 +85,14 @@ const GridPage = ({ data }) => {
               classNames={searchBar}
               placeholder="Search for Title, Actor, Director..."
               onChange={(value) =>
-                searchFilter(value, dataQuery(selected, { data })).length !== 0
-                  ? setTable(searchFilter(value, dataQuery(selected, { data })))
+                searchFilter(value, dataQuery(selected, { data }, sliderValue))
+                  .length !== 0
+                  ? setTable(
+                      searchFilter(
+                        value,
+                        dataQuery(selected, { data }, sliderValue)
+                      )
+                    )
                   : setTable(nodes)
               }
             />
@@ -135,7 +130,8 @@ const GridPage = ({ data }) => {
               <button
                 style={{ width: "40%", borderRadius: "8px" }}
                 onClick={() => {
-                  setTable(dataQuery(selected, { data }));
+                  setShowDropdown(!showDropdown);
+                  setTable(dataQuery(selected, { data }, sliderValue));
                 }}
               >
                 Apply
@@ -157,30 +153,59 @@ const GridPage = ({ data }) => {
               </button>
             </Grid>
             {filter.map((opt) => {
-              return (
-                <Grid xs={6} item={true} key={`${opt["label"]}-12`}>
-                  <div
-                    key={`${opt["label"]}-select-picker`}
-                    style={{ textAlign: "center" }}
-                  >
-                    {opt["label"]}
-                  </div>
-                  <Select
-                    className={reactSelectContainer}
-                    classNamePrefix="react-select"
-                    options={opt["options"]}
-                    isMulti
-                    closeMenuOnSelect={false}
-                    isSearchable
-                    placeholder={`Ex: ${opt["options"][0]["label"]}, ${opt["options"][1]["label"]}`}
-                    onChange={(e) => {
-                      setSelected((prevState) =>
-                        selectedOptions(prevState, e, opt["label"])
-                      );
-                    }}
-                  ></Select>
-                </Grid>
-              );
+              if (!opt["Runtime"]) {
+                return (
+                  <Grid xs={12} md={6} item={true} key={`${opt["label"]}-12`}>
+                    <div
+                      key={`${opt["label"]}-select-picker`}
+                      style={{ textAlign: "center" }}
+                    >
+                      {opt["label"]}
+                    </div>
+                    <Select
+                      className={reactSelectContainer}
+                      classNamePrefix="react-select"
+                      options={opt["options"]}
+                      isMulti
+                      closeMenuOnSelect={false}
+                      isSearchable
+                      placeholder={
+                        opt["label"] === "Genre"
+                          ? "Ex: Animated, Horror"
+                          : `Ex: ${opt["options"][0]["label"]}`
+                      }
+                      onChange={(e) => {
+                        setSelected((prevState) =>
+                          selectedOptions(prevState, e, opt["label"])
+                        );
+                      }}
+                    ></Select>
+                  </Grid>
+                );
+              } else {
+                return (
+                  <Grid xs={12} md={6} item={true} key={`runtime-12`}>
+                    <div key={`runtime-slider`} style={{ textAlign: "center" }}>
+                      Runtime
+                    </div>
+                    <Slider
+                      min={opt["Runtime"][0]}
+                      max={opt["Runtime"][opt["Runtime"].length - 1]}
+                      onChange={handleChange}
+                      style={{
+                        width: "95%",
+                        marginLeft: "2.5%",
+                        color: "#55CBCD",
+                      }}
+                      value={sliderValue}
+                      valueLabelDisplay="auto"
+                      valueLabelFormat={(x) => {
+                        return `${x} min`;
+                      }}
+                    />
+                  </Grid>
+                );
+              }
             })}
           </Grid>
         ) : null}
@@ -191,81 +216,9 @@ const GridPage = ({ data }) => {
     </div>
   );
 };
-  return (
-    <div>
-      <Layout pageTitle = "Movies :)">
-        <Row className={reactGrid}>
-          <Col md={2}></Col>
-          <Col md={4} style={{textAlign:"center"}}>
-            <SearchField
-              classNames={searchBar}
-              placeholder='Search for Title, Actor, Director...'
-              onChange={(value) => searchFilter(value, dataQuery(selected, { data }, sliderValue)).length !== 0 ? setTable(searchFilter(value, dataQuery(selected, { data }, sliderValue))) : setTable(nodes)}
-            />
-          </Col>
-          <Col md={4}>
-            <button
-              style={{borderRadius: "8px"}}
-              onClick={() => {setShowDropdown(!showDropdown);}}
-            >
-              {showDropdown ? "Hide" : "Filters"} &#8597;
-            </button>
-          </Col>
-        </Row>
-        { showDropdown ? 
-          <Grid container style={{marginBottom: "1rem", background: "#d4f0f0", paddingTop: "0.5rem"}}>
-            <Grid xs={6} item={true} key={1} style={{marginBottom: "0.5rem",  textAlign: "right", paddingRight: "0.5rem"}}>
-              <button style={{width: "40%", borderRadius: "8px"}} onClick={() => {setShowDropdown(!showDropdown); setTable(dataQuery(selected, { data }, sliderValue))}}>Apply</button>
-            </Grid>
-            <Grid xs={6} item={true} key={2} style={{marginBottom: "0.5rem", paddingLeft: "0.5rem"}}>
-              <button style={{width: "40%", borderRadius: "8px"}} onClick={() => {resetFilter()}}>Reset</button>
-            </Grid>
-            {filter.map((opt) => {
-              if (!opt["Runtime"]) {
-                return (
-                  <Grid xs={12} md={6} item={true} key={`${opt["label"]}-12`}>
-                    <div key={`${opt["label"]}-select-picker`} style={{textAlign: "center"}}>{opt["label"]}</div>
-                    <Select
-                      className={reactSelectContainer}
-                      classNamePrefix="react-select"
-                      options={opt["options"]}
-                      isMulti
-                      closeMenuOnSelect={false}
-                      isSearchable
-                      placeholder={opt["label"] === "Genre" ? "Ex: Animated, Horror" : `Ex: ${opt["options"][0]["label"]}`}
-                      onChange={(e) => {setSelected((prevState) => selectedOptions(prevState, e, opt["label"]))}}
-                      >
-                    </Select>
-                  </Grid>
-                )
-              } else {
-                return (
-                  <Grid xs={12} md={6} item={true} key={`runtime-12`}>
-                    <div key={`runtime-slider`} style={{textAlign: "center"}}>Runtime</div>
-                    <Slider
-                      min={opt["Runtime"][0]}
-                      max={opt["Runtime"][opt["Runtime"].length - 1]}
-                      onChange={handleChange}
-                      style={{width: "95%", marginLeft: "2.5%", color: "#55CBCD"}}
-                      value={sliderValue}
-                      valueLabelDisplay="auto"
-                      valueLabelFormat={(x) => {return `${x} min`}}
-                    />
-                  </Grid>
-                )
-              }
-            })} 
-          </Grid>: null}
-        <Row>
-          <MovieGrid nodes={table}/>
-        </Row>
-      </Layout>
-    </div>
-  )
-}
 
 export const query = graphql`
-  query  {
+  query {
     genre: allMovieMovieMoviesXlsxMasterlist {
       distinct(field: Genre)
     }
@@ -335,5 +288,6 @@ export const query = graphql`
         id
       }
     }
-  }`
-export default GridPage
+  }
+`;
+export default GridPage;
