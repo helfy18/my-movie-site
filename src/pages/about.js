4e498a37c7d4a437f943b6e973c944d1b472db0a
Johnathan <jhelfric@uvic.ca>
2024-04-16 21:55:41 -0700

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,19 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About">
-        <p>
-            Hello, our names are Johnathan and Dani, and we built this website to showcase our movie ratings.
-            We will consistently be adding new ratings as we watch them.<br/><br/>
-            Look out for site upgrades as well! Every Saturday morning MST there will be an update to the blog
-            that shows the newly added movies to the site over the past week!
-        </p>
+      <p>
+        Hello, my name is Johnathan, and I built this website to showcase my
+        movie ratings. I will consistently be adding new ratings as I watch
+        them.
+        <br />
+        <br />
+        Look out for site upgrades as well!
+      </p>
     </Layout>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
