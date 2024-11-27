5fb3ae16d1b9c10a752b07e4b6e6f572ab7984b1
Johnathan <jhelfric@uvic.ca>
2024-11-27 08:56:26 -0800

    content update

--- src/data/movieScript.py
+++ src/data/movieScript.py
@@ -31,79 +31,16 @@ apikey = config.apikey
 currentTime = round(time.time() * 1000)
 
 for index, row in enumerate(ws.iter_rows(values_only=True)):
-    # if index == 950:
-    #    apikey = config.apikey2
+    if index == 950:
+       apikey = config.apikey2
     if index >= 1:
         # skip entries already filled, comment out if full update required
-        if ws[index + 1][plot].value:
-            continue
+        # if ws[index + 1][plot].value:
+        #     continue
 
         # title and year for search
         title = ws[index + 1][titleIndex].value
         year = ws[index + 1][yearIndex].value
-
-        # Special Cases
-        if title == "The Black Phone":
-            year = '2021'
-        if title == "Monty Python's Life of Brian":
-            title = "Life of Brian"
-        if title == "The Lion King 1 1/2":
-            title = "The Lion King 3"
-        if "Batman v Superman" in title:
-            title = "Batman v Superman"
-        if title == "Mom and Dad":
-            year = '2017'
-        if title == "Aladdin 2: The Return of Jafar":
-            title = "The Return of Jafar"
-        if title == "Fant4stic":
-            title = "Fantastic Four"
-        if title == "Cyrano":
-            year = '2021'
-        if "&" in title:
-            title = title.replace("&", "%26")
-        if title == "Tiptoes":
-            year = '2002'
-        if title == 'Glass Onion: A Knives Out Mystery':
-            title = 'Glass Onion'
-        if title == 'Marcel the Shell with Shoes On':
-            year = '2021'
-        if title == 'M3GAN':
-            year = '2022'
-        if title == 'Heathers':
-            year = '1988'
-        if "Mad Max 2" in title:
-            title = "The Road Warrior"
-        if 'Naruto Shippuden the Movie' in title:
-            title = 'Naruto Shippuden'
-        if title == 'Expend4bles':
-            title = 'The Expendables 4'
-        if title == 'Friday the 13th Part III':
-            title = 'Friday the 13th: Part 3'
-
-        # submit api request
-
-        if not ws[index + 1][ratings].value:
-            omdb = requests.get(f'http://www.omdbapi.com/?apikey={apikey}&t={title}&y={year}&type=movie').json()
-            rotten = [rating['Value'] for rating in omdb["Ratings"] if rating['Source'] == 'Rotten Tomatoes']
-            ws[index + 1][rt].value = rotten[0] if len(rotten) > 0 else 'N/A'
-            internationalMovieDB = [rating['Value'] for rating in omdb["Ratings"] if rating['Source'] == 'Internet Movie Database']
-            ws[index + 1][imdb].value = internationalMovieDB[0] if len(internationalMovieDB) > 0 else 'N/A'
-            mc = [rating['Value'] for rating in omdb["Ratings"] if rating['Source'] == 'Metacritic']
-            ws[index + 1][metacritic].value = mc[0] if len(mc) > 0 else 'N/A'
-
-            ws[index + 1][ratings].value = json.dumps(omdb["Ratings"])
-            ws[index + 1][rated].value = omdb["Rated"]
-        
-        if 'Naruto Shippuden' in title:
-            title = ws[index + 1][titleIndex].value
-        if title == 'Blades of Glory':
-            year = '2007'
-        if title == "Tiptoes":
-            year = '2003'
-        if title == 'Heathers':
-            year = '1989'
-        if title == 'Spiral':
-            title = 'Spiral: From the Book of Saw'
         
         if not ws[index + 1][tmdbid].value:
             search = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={config.tmdbkey}&query={title}&year={year}').json()
@@ -113,7 +50,7 @@ for index, row in enumerate(ws.iter_rows(values_only=True)):
             ws[index + 1][poster].value = f'https://image.tmdb.org/t/p/w500{path}'
             ws[index + 1][tmdbid].value = tmdbcode
 
-            ws[index + 1][millis].value = currentTime
+            ws[index + 1][millis].value = str(currentTime)
         else:
             tmdbcode = ws[index + 1][tmdbid].value
 
@@ -143,6 +80,7 @@ for index, row in enumerate(ws.iter_rows(values_only=True)):
             boxofficeTotal = movieInfo['revenue']
         except KeyError:
             boxofficeTotal = 'N/A'
+        imdbid = movieInfo["imdb_id"]
         ws[index + 1][plot].value = movieInfo["overview"]
         ws[index + 1][boxoffice].value = f"{boxofficeTotal:,}"
         ws[index + 1][budget].value = f"{movieInfo['budget']:,}"
@@ -168,7 +106,6 @@ for index, row in enumerate(ws.iter_rows(values_only=True)):
            selected_trailer = ''
         ws[index + 1][trailer].value = f'https://www.youtube.com/embed/{selected_trailer["key"]}' if selected_trailer != '' else ''
 
-
         url = f'https://api.themoviedb.org/3/movie/{tmdbcode}/rating'
         headers = {'Content-Type': 'application/json;charset=utf8', 'Authorization': f'Bearer {config.tmdbtoken}'}
         if ws[index + 1][1].value:
@@ -178,6 +115,18 @@ for index, row in enumerate(ws.iter_rows(values_only=True)):
             data = {"value": value}
             response = requests.post(url, headers=headers, json=data).json()
 
-        print(title, year)
+        # OMDB SECTION
+        omdb = requests.get(f'http://www.omdbapi.com/?apikey={apikey}&i={imdbid}&type=movie').json()
+        rotten = [rating['Value'] for rating in omdb["Ratings"] if rating['Source'] == 'Rotten Tomatoes']
+        ws[index + 1][rt].value = rotten[0] if len(rotten) > 0 else 'N/A'
+        internationalMovieDB = [rating['Value'] for rating in omdb["Ratings"] if rating['Source'] == 'Internet Movie Database']
+        ws[index + 1][imdb].value = internationalMovieDB[0] if len(internationalMovieDB) > 0 else 'N/A'
+        mc = [rating['Value'] for rating in omdb["Ratings"] if rating['Source'] == 'Metacritic']
+        ws[index + 1][metacritic].value = mc[0] if len(mc) > 0 else 'N/A'
+
+        ws[index + 1][ratings].value = json.dumps(omdb["Ratings"])
+        ws[index + 1][rated].value = omdb["Rated"]
+
+        print(title, year, index)
         
 wb.save('MovieMovieMovies.xlsx')
